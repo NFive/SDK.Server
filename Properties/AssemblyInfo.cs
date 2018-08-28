4ad69c3aa66eeddd30d51ba84a6c4232cf52eb31
Joe Biellik <joebiellik@gmail.com>
2018-08-28 16:07:20 +0100

    Update project metadata

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,15 +1,16 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("IgiCore Server SDK")]
+[assembly: AssemblyTitle("NFive Server SDK")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("IgiCore")]
-[assembly: AssemblyProduct("IgiCore SDK")]
-[assembly: AssemblyCopyright("Copyright © IgiCore 2018")]
+[assembly: AssemblyCompany("NFive")]
+[assembly: AssemblyProduct("NFive SDK")]
+[assembly: AssemblyCopyright("Copyright © NFive 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("5a53d22b-87f1-4b16-b54b-d0a8aeac8e5d")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
