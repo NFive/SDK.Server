9e400204237bec9645fcaafcee6b4d6205bd5bb0
Igi <igirisujinsenpai@gmail.com>
2018-09-08 23:56:43 +0100

    Added methods stubs for Request/OnRequest

--- Events/IEventManager.cs
+++ Events/IEventManager.cs
@@ -19,6 +19,7 @@ namespace NFive.SDK.Server.Events
 
 		void On<T1, T2, T3, T4, T5>(string @event, Action<T1, T2, T3, T4, T5> action);
 
+
 		void Raise(string @event);
 
 		void Raise<T>(string @event, T p1);
@@ -43,5 +44,29 @@ namespace NFive.SDK.Server.Events
 		Task RaiseAsync<T1, T2, T3, T4>(string @event, T1 p1, T2 p2, T3 p3, T4 p4);
 
 		Task RaiseAsync<T1, T2, T3, T4, T5>(string @event, T1 p1, T2 p2, T3 p3, T4 p4, T5 p5);
+
+
+		void OnRequest<TReturn>(string @event, Func<TReturn> action);
+
+		void OnRequest<T1, TReturn>(string @event, Func<T1, TReturn> action);
+
+		void OnRequest<T1, T2, TReturn>(string @event, Func<T1, T2, TReturn> action);
+
+		void OnRequest<T1, T2, T3, TReturn>(string @event, Func<T1, T2, T3, TReturn> action);
+
+		void OnRequest<T1, T2, T3, T4, TReturn>(string @event, Func<T1, T2, T3, T4, TReturn> action);
+
+		void OnRequest<T1, T2, T3, T4, T5, TReturn>(string @event, Func<T1, T2, T3, T4, T5, TReturn> action);
+
+
+		TReturn Request<T1, TReturn>(string @event, T1 arg);
+
+		TReturn Request<T1, T2, TReturn>(string @event, T1 arg);
+
+		TReturn Request<T1, T2, T3, TReturn>(string @event, T1 arg);
+
+		TReturn Request<T1, T2, T3, T4, TReturn>(string @event, T1 arg);
+
+		TReturn Request<T1, T2, T3, T4, T5, TReturn>(string @event, T1 arg);
 	}
 }
