a15950adf26ae618e1f6d1b62f990c0d24fba0d8
Igi <brad@igi.codes>
2018-12-08 22:16:15 +0000

    Update SDK.Core ref and added Boot/Server Event constants

---
+++ Events/BootEvents.cs
@@ -0,0 +1,9 @@
+﻿namespace NFive.SDK.Server.Events
+{
+	public static class BootEvents
+	{
+		public const string GetTime = "nfive:server:boot:getTime";
+		public const string GetLastTime = "nfive:server:boot:getLastTime";
+		public const string GetLastActiveTime = "nfive:server:boot:getLastActiveTime";
+	}
+}
---
+++ Events/ServerEvents.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NFive.SDK.Server.Events
+{
+	public static class ServerEvents
+	{
+		public const string ServerInitialized = "nfive:server:serverInitialized";
+	}
+}
