088f11ee69b1e479938f6dbe8ff2b33fc9da8d35
Amir Halloul <amirhalloul@gmail.com>
2018-12-26 17:11:21 +0100

    Added ability to call RPCs for a single client.

--- Rpc/IRpcTrigger.cs
+++ Rpc/IRpcTrigger.cs
@@ -13,5 +13,12 @@ namespace NFive.SDK.Server.Rpc
 		/// </summary>
 		/// <param name="payloads">The payloads to send with the event.</param>
 		void Trigger(params object[] payloads);
+
+		/// <summary>
+		/// Triggers the event for the specified client with the specified payloads.
+		/// </summary>
+		/// <param name="client">The client to send the event to.</param>
+		/// <param name="payloads">The payloads to send with the event.</param>
+		void Trigger(IClient client, params object[] payloads);
 	}
 }
