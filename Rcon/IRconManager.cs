ba9946d3dd122bbb5548c340ef9ef94b9310774b
Joe Biellik <joebiellik@gmail.com>
2018-12-29 18:58:54 +0000

    Cleanup

--- Rcon/IRconManager.cs
+++ Rcon/IRconManager.cs
@@ -1,11 +1,13 @@
-﻿using System;
+﻿using JetBrains.Annotations;
+using System;
 
 namespace NFive.SDK.Server.Rcon
 {
+	[PublicAPI]
 	public interface IRconManager
 	{
-		void Register<T>(string command, Action<T> callback);
-
 		void Register(string command, Action callback);
+
+		void Register<T>(string command, Action<T> callback);
 	}
 }
