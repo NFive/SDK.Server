9445dc26cbf6175c6407454d0eebdd5989590692
Joe Biellik <joebiellik@gmail.com>
2019-10-02 22:25:02 +0100

    Separate On and OnRequest

--- Communications/ICommunicationReceiveClient.cs
+++ Communications/ICommunicationReceiveClient.cs
@@ -16,5 +16,17 @@ namespace NFive.SDK.Server.Communications
 		void On<T1, T2, T3, T4>(Action<ICommunicationMessage, T1, T2, T3, T4> callback);
 
 		void On<T1, T2, T3, T4, T5>(Action<ICommunicationMessage, T1, T2, T3, T4, T5> callback);
+
+		void OnRequest(Action<ICommunicationMessage> callback);
+
+		void OnRequest<T>(Action<ICommunicationMessage, T> callback);
+
+		void OnRequest<T1, T2>(Action<ICommunicationMessage, T1, T2> callback);
+
+		void OnRequest<T1, T2, T3>(Action<ICommunicationMessage, T1, T2, T3> callback);
+
+		void OnRequest<T1, T2, T3, T4>(Action<ICommunicationMessage, T1, T2, T3, T4> callback);
+
+		void OnRequest<T1, T2, T3, T4, T5>(Action<ICommunicationMessage, T1, T2, T3, T4, T5> callback);
 	}
 }
