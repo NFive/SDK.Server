219a51b078cb905a1e94ade95b645322844c3f9f
Joe Biellik <joebiellik@gmail.com>
2019-10-11 21:11:41 +0100

    Improve extension methods

--- Extensions/PositionExtensions.cs
+++ Extensions/PositionExtensions.cs
@@ -6,23 +6,6 @@ namespace NFive.SDK.Server.Extensions
 	[PublicAPI]
 	public static class PositionExtensions
 	{
-		/// <summary>
-		/// Coverts to <see cref="Vector3" />.
-		/// </summary>
-		/// <param name="pos">The <see cref="Position" /> to convert.</param>
-		/// <returns>The <see cref="Vector3" />.</returns>
-		public static Vector3 ToVector3(this Position pos) => new Vector3
-		{
-			X = pos.X,
-			Y = pos.Y,
-			Z = pos.Z
-		};
-
-		/// <summary>
-		/// Converts to CitizenFX Vector3.
-		/// </summary>
-		/// <param name="position">The <see cref="Position" /> to convert.</param>
-		/// <returns>The CitizenFX Vector3.</returns>
 		public static CitizenFX.Core.Vector3 ToCitVector3(this Position position) => new CitizenFX.Core.Vector3(position.X, position.Y, position.Z);
 	}
 }
